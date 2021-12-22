0cfdbf432ac1b70ebca84ba63846b44f2092a8e5
Kyle Conrad <kyle@kyleconrad.io>
2021-12-22 11:38:37 +0000

    Desaturate all images and solids to be grayscale

--- plugin.ts
+++ plugin.ts
@@ -2,24 +2,30 @@ function clone(val) {
   return JSON.parse(JSON.stringify(val))
 }
 
-function getLightnessOfRGB(rgbString) {
-  const rgbIntArray = rgbString.replace(/ /g, '').slice(4, -1).split(',').map(e => parseInt(e));
+function getLightnessOfRGB(r, g, b) {
 
-  const highest = Math.max(...rgbIntArray);
-  const lowest = Math.min(...rgbIntArray);
+  const highest = Math.max(r, g, b);
+  const lowest = Math.min(r, g, b);
 
   return (highest + lowest) / 2 / 255;
 }
 
-figma.currentPage.selection.map(n => {
-  if (!!n.fills[0] && n.fills[0].type === "SOLID") {
+figma.currentPage.findAll().map(n => {
+  if (!!n && !!n.fills[0] && n.fills[0].type === "SOLID") {
     const fills = clone(n.fills);
     const originalFills = fills
     const fill = fills[0];
-    const grayVal = getLightnessOfRGB(`rgb(${fill.color.r * 255}, ${fill.color.r * 255}, ${fill.color.r * 255})`);
+    const grayVal = getLightnessOfRGB(fill.color.r * 255, fill.color.g * 255, fill.color.b * 255);
     fill.color.r = grayVal
     fill.color.g = grayVal
     fill.color.b = grayVal
+    n.fills = fills
+  }
+
+  if (!!n && !!n.fills[0] && n.fills[0].type === "IMAGE") {
+    const fills = clone(n.fills);
+    const fill = fills[0];
+    fill.filters.saturation = -1
     n.fills = fills
   }
 })
